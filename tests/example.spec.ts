321a9f5187eb56c31c8d9c5d290034993bfef3df
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2025-03-10 13:34:32 +0000

    Setup

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,9 @@
-import { test, expect } from '@playwright/test';
+import {expect, test} from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test(`First test`, async ({ page }) => {
+    await page.goto(`https://www.gov.uk/calculate-your-holiday-entitlement`);
+    await expect(page.locator(`.govuk-heading-xl`))
+        .toHaveText(`Calculate holiday entitlement`);
+    // Continue me!
+    // How would you click the continue button?
 });
